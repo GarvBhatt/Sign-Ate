713f74670f8d20ec75b24b70fb77e7f3ae18d997
Garv <garvbhatt2085@gmail.com>
2023-11-10 18:45:08 +0530

    4th commit

--- src/MyComponent/Header.jsx
+++ src/MyComponent/Header.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
-import { Container, Dropdown, Navbar, Nav } from "react-bootstrap";
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Header = (props) => {
-  const [showDropdown, setshowDropdown] = useState(false);
-  const categories = ["General", "Business", "Entertainment", "Health", "Science", "Sports", "Technology"];
   const { title } = props;
 
   const headerStyle = {
